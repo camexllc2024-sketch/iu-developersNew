9eb99f803a8037e74c7532b057280700605b8f5c
camexllc2024 <Camexllc2024@gmail.com>
2026-05-03 17:08:39 +0200

    Fix Vite build by using ES modules for API functions

--- api/send-email.js
+++ api/send-email.js
@@ -1,6 +1,6 @@
-const nodemailer = require('nodemailer');
+import nodemailer from 'nodemailer';
 
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   // Add CORS headers
   res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('Access-Control-Allow-Origin', '*');
